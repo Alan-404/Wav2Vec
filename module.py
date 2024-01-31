4aaa70fe0598298fe01d164303c3b0478b20d173
Alan-404 <nguyentri.alan@gmail.com>
2024-01-31 17:07:34 +0700

    .

--- module.py
+++ module.py
@@ -34,7 +34,7 @@ class Wav2VecModule(L.LightningModule):
         self.criterion = ConformerCriterion(blank_id=pad_token)
         self.metric = ConformerMetric()
 
-        # self.save_hyperparameters(ignore=['pad_token', 'metric_fx'])
+        self.save_hyperparameters(ignore=['pad_token', 'metric_fx'])
 
     def training_step(self, batch: Tuple[torch.Tensor], _: int):
         inputs = batch[0]
